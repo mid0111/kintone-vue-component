cd58f909e9c78ecb27ce9637ddee60f20915ffe1
mid0111 <m.tajima0111@gmail.com>
2020-06-03 12:36:04 +0900

    デモにコードのリンクを追加

--- src/demo/ComponentTitle.vue
+++ src/demo/ComponentTitle.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="title-container">
-    <a :href="`#${title}`" class="title-link">
-      <h2 :id="title">{{ title }}</h2>
-    </a>
+  <div>
+    <div class="title-container">
+      <a :href="`#${title}`" class="title-link">
+        <h2 :id="title">{{ title }}</h2>
+      </a>
+    </div>
+    <a class="demo-link" :href="sourceLink" target="_blank">コードを見る</a>
   </div>
 </template>
 
 <script lang="ts">
+import { upperFirst, camelCase } from 'lodash';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class KButton extends Vue {
   @Prop() private title!: string;
+
+  get sourceLink(): string {
+    const pascalCaseTitle = upperFirst(camelCase(this.title));
+    return `https://github.com/mid0111/kintone-vue-component/blob/master/src/demo/${pascalCaseTitle}Demo.vue`;
+  }
 }
 </script>
 
@@ -20,11 +29,15 @@ export default class KButton extends Vue {
   border-bottom: 1px solid #ddd;
   border-left: 4px solid #3498db;
   padding: 8px 10px;
-  margin-bottom: 40px;
+  margin-bottom: 20px;
   margin-top: 80px;
 }
 
 .title-link h2 {
   margin: 0;
+}
+
+.demo-link {
+  padding-left: 20px;
 }
 </style>
